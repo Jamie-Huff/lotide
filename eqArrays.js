01f7a5c9fb9dde0d47481f0ee08365ae78f7751a
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-23 20:04:39 +0000

    fixed error

--- eqArrays.js
+++ eqArrays.js
@@ -17,5 +17,4 @@ const eqArrays = function(array1, array2) {
   }
   return true
 }
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true); // => should PASS

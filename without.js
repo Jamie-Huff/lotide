e216e6e8f07e069f33dd09e06bca6dee5938cfb0
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-23 16:35:17 +0000

    function to get the middle value(s) depending on an even or odd number of array length

--- without.js
+++ without.js
@@ -31,3 +31,8 @@ const without = function(array1, array2) {
   return arrayFinal
 }
 console.log(assertArrayEqual(without([2, 3, 1], [1]), [2, 3]))
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(assertArrayEqual(words, ["hello", "world", "lighthouse"]));

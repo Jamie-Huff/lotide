248a4051ebc20ed5dfc41f5473cf978f15f51c21
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-23 18:20:49 +0000

    lists locations of all letters in array format

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const eqArrays = function(array1, array2) {
+  let finalarray = []
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      finalarray.push(array1[i])
+    } else if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const assertArrayEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    return "These arrays have equal values."
+  } 
+  return "The values are not equal."
+}
+
+
+const letterPositions = function(string) {
+  let finalLetterCountObject = {}
+  stringArray = string.split('')
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] === " ") {
+      continue
+    }
+    if (finalLetterCountObject[stringArray[i]]) {
+      finalLetterCountObject[stringArray[i]].push(i)
+    }
+    if (!finalLetterCountObject[stringArray[i]]) {
+      finalLetterCountObject[stringArray[i]] = [i]
+    }
+  }
+
+  return finalLetterCountObject
+}
+
+
+
+console.log(letterPositions("big string"))
+let letterFinalObject = letterPositions("big string")
+
+console.log(assertArrayEqual(letterFinalObject["i"], [ 1, 7]))

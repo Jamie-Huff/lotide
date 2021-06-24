2fd40dbedac2083b4601bae042958bc82294a476
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-24 21:12:59 +0000

    find a specific value of an object based off of our callback

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = (object, callback) => {
+    for (const value in object) {
+      if (callback(object[value])) {
+        return value
+    }
+  }
+}
+
+
+const results1 = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2) // => "noma"
+
+console.log(assertEqual(results1, "noma"))

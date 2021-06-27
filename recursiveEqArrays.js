1791e8a42260ecd26218d724a5c0a37b35890a20
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-27 17:35:02 +0000

    calculate for recursive arrays

---
+++ recursiveEqArrays.js
@@ -0,0 +1,22 @@
+function eqArrays(actual, expected) {
+  if (!expected || !actual) {
+    return false
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (Array.isArray(actual[i])) {
+      if (!eqArrays(actual[i], expected[i])) {
+        return false
+      } else {
+        continue
+      }
+    }
+    if (actual[i] !== expected[i]) {
+      return false
+     }
+  }
+  return true
+}
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
+console.log(eqArrays([[2, 3, [[[[1]]]]], [4]], [[2, 3, [[[[1]]]]], [4]])) // => false

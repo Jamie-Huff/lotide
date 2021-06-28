fb7646838d9a72e959711ec5fb3bea1eb53e29f0
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-28 17:41:27 +0000

    implemented test options to different test file in ./tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,12 @@
-const eqArrays = function(array1, array2) {
-  let finalarray = []
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      finalarray.push(array1[i])
-    } else if (array1[i] !== array2[i]) {
-      return false
-    }
-  }
-  return true
-}
+const eqArrays = require("./eqArrays")
 
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    return "These arrays have equal values."
+    return console.log("These arrays have equal values.")
   } 
-  return "The values are not equal."
+  return console.log("The values are not equal.")
 
 }
 
-console.log(assertArrayEqual([1, 2, 3], [1, 2, 3]))
+module.exports = assertArrayEqual
+

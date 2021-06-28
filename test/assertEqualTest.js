237b61ab3bd3065fa37261d98e6aeeb1bc8d13d5
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-28 17:03:03 +0000

    test files for assertEqual

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual')
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('this dog', 'this dog');
+assertEqual('nice puppy', 'NiCe PuPpY');
+assertEqual(15, 15);
+assertEqual(15, 2102103120);

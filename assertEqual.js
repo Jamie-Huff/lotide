4cbf4e777667a744267a3e023de74513532e7467
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-06-28 17:00:59 +0000

    added in automated tests lotide/test/assertEqualTest

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('this dog', 'this dog');
-assertEqual('nice puppy', 'NiCe PuPpY');
-assertEqual(15, 15);
-assertEqual(15, 2102103120);
+module.exports = assertEqual
